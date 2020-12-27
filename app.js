49a19f7dc938aef72146bf379c87d1d4157a6da1
Muhammad Zohaib <zohaibfas@gmail.com>
2020-12-27 13:32:22 +0500

    except new game everything is ok

--- app.js
+++ app.js
@@ -8,3 +8,78 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+
+
+document.querySelector('.dice').style.display = 'none'; 
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+// adding funtionality of the Roll button
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+
+    // changing the player
+
+    if (dice !==1 ) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }else{
+        nextPlayer();
+    
+    }
+});
+
+//adding funtionality of the Hold Button
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    
+    scores[activePlayer] += roundScore;
+    //updating the UI
+    document.querySelector('#score-'+ activePlayer).textContent = scores[activePlayer];
+
+    //checking the winner
+    if (scores[activePlayer] >= 30){
+        document.querySelector('#name-'+ activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+
+    }else{
+        //Next Player
+        nextPlayer();
+    }
+    
+});
+
+function nextPlayer() {
+
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+        
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('dice').style.display = 'none';
+};
+
+//adding funtionalites of new button
